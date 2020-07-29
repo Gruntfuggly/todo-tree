b1ce1b197710339c1b58364446092d4867fc0c3f
Nigel Scott <nige@zaonce.com>
2020-07-29 22:14:20 +0100

    sq webpack

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,7 @@ const path = require( 'path' );
 const config = {
     target: 'node', // vscode extensions run in a Node.js-context 📖 -> https://webpack.js.org/configuration/node/
 
-    entry: './extension.js', // the entry point of this extension, 📖 -> https://webpack.js.org/configuration/entry-context/
+    entry: './src/extension.js', // the entry point of this extension, 📖 -> https://webpack.js.org/configuration/entry-context/
     output: {
         // the bundle is stored in the 'dist' folder (check package.json), 📖 -> https://webpack.js.org/configuration/output/
         path: path.resolve( __dirname, 'dist' ),
